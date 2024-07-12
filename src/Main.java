92f6a53ec29937deb5576c3254dd0e49640188f1
OyarzabalCassio <cassio1357@gmail.com>
2024-07-11 23:28:59 -0300

    Exercicio com Record, Conexao com API, Gson e gravação em arquivo

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,11 @@ public class Main {
         FileWriter fw = new FileWriter(file);
         Scanner sc = new Scanner(System.in);
 
-        
+
         System.out.println("Digite um CEP:");
         String cep = sc.nextLine();
         ViaCepAPI viaCepAPI = new ViaCepAPI(cep);
-        String resposta = viaCepAPI.retornaResposta();
-        ViaCepRecord viaCep = gson.fromJson(resposta, ViaCepRecord.class);
+        ViaCepRecord viaCep = gson.fromJson(viaCepAPI.retornaResposta(), ViaCepRecord.class);
         ViaCEPImpl viaCEPImpl = new ViaCEPImpl(viaCep);
         fw.write(gson.toJson(viaCEPImpl));
         fw.close();
